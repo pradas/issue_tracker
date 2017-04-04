c65e5d6467505601dabcc0dab93d1dee340eb1ba
Gerard Pradas <gerard.pradas.san.jose@est.fib.upc.edu>
2017-04-04 13:30:34 +0000

    improved style with bootstrap

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,8 @@
 class Issue < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :user_id, presence: true
+  validates :kind, presence: true
+  validates :priority, presence: true
 end
