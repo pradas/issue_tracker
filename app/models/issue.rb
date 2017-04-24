e728e34ab7e7a2e3b06491a6a82e23942bc4899f
Adrián Ruiz Cordón <ruiza3e@gmail.com>
2017-04-24 10:20:41 +0000

    Asterisks + Empty description

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,7 +3,6 @@ class Issue < ApplicationRecord
   has_many :comments
   
   validates :title, presence: true
-  validates :description, presence: true
   validates :user_id, presence: true
   validates :kind, presence: true
   validates :priority, presence: true
