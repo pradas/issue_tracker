cd6e2a5e40097946df1d6d98876f1d7d9520bcd8
Adrian Borrego <adrian.borrego@est.fib.upc.edu>
2017-04-24 10:48:01 +0000

    Modificacion de worfklow para que no salga el estado actual

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,4 +1,5 @@
 class Resume < ActiveRecord::Base
    mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
    validates :name, presence: true 
+   validates :attachment, presence: true
 end
