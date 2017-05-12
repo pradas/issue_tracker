f3d0d80af65cf81337c129a2edd8d6a3a7fa491b
Adrian Borrego <adrian.borrego@est.fib.upc.edu>
2017-05-12 14:34:06 +0000

    call get issues comenzada

--- app/controllers/api_issues_controller.rb
+++ app/controllers/api_issues_controller.rb
@@ -1,2 +1,6 @@
-class ApiIssuesControllerController < ApplicationController
+class ApiIssuesController < ApplicationController
+  def index
+    @issues = Issue.all
+    render status :ok, file: "api/issues/index.json.jbuilder"
+  end
 end
